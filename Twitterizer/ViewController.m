d91771dfd7dd02179af6b6acd539133cc9952cc5
ALIREZA TABRIZI <alirezatabrizi@m.local>
2016-03-16 16:03:15 -0700

    step 2

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *characterCounts;
+
 
 @end
 
@@ -23,5 +26,26 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)ontwitterizeButtonPushed:(UIButton *)sender {
+    NSString *userInputText = self.textView.text;
+    NSUInteger length = userInputText.length;
+   // NSLog(@"%lu", length);
+    NSMutableString *vowelsRemoved = [NSMutableString new];
+    NSCharacterSet *vowel = [NSCharacterSet characterSetWithCharactersInString:@"aeiouAEIOU"];
+    for (int i = 0; i < length-1; i++) {
+        if (![vowel characterIsMember:[userInputText characterAtIndex:i]]) {
+            [vowelsRemoved appendString: [NSString stringWithFormat: @"%c",[userInputText characterAtIndex:i]]];
+        }
+    }
+    self.textView.text = vowelsRemoved;
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView
+{
+    NSString *userInputText = self.textView.text;
+    NSUInteger length = userInputText.length;
+    self.characterCounts.text = [NSString stringWithFormat:@"%lu", length];
+}
+
 
 @end
