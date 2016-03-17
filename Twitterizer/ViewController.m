ea22c2d90d3e532c20637c51e824f048a3b24adb
ALIREZA TABRIZI <alirezatabrizi@m.local>
2016-03-16 19:39:15 -0700

    Final commit

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -19,7 +19,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.textView.delegate = self;
+    
+    self.textView.text = @"";
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,12 +43,93 @@
     self.textView.text = vowelsRemoved;
 }
 
-- (void)textViewDidBeginEditing:(UITextView *)textView
-{
+-(void)textViewDidChange:(UITextView *)textView{
     NSString *userInputText = self.textView.text;
     NSUInteger length = userInputText.length;
     self.characterCounts.text = [NSString stringWithFormat:@"%lu", length];
 }
+
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    //int limit = 140;
+    if (self.textView.text.length + text.length>140){
+        return NO;
+    //return !([textView.text length]>=limit);
+    }
+    return YES;
+}
+
+int numHashtagButtonPresses = 0;
+
+//add Hashtag
+- (IBAction)onHashtaButtonPressed:(UIButton *)sender {
+    numHashtagButtonPresses++;
+    NSString *allText = self.textView.text;
+    NSArray *wordsArray = [allText componentsSeparatedByString:@" "];
+    
+    NSMutableString *newText = [NSMutableString new];
+    //NSString *newText2 = @"";
+    
+    for (int i = 0; i < wordsArray.count; i++) {
+        NSString *word = wordsArray[i];
+        if (i % 2 == numHashtagButtonPresses %2) {
+            //even: do nothing
+            word = [self addHastagToWord:word];
+        }
+        //version 1
+        [newText appendString:word];
+        [newText appendString:@" "];
+    }
+        //version 2
+        //newText2 = [NSString stringWithFormat:@"%@%@ ", newText2, word];
+        newText = [newText substringToIndex:(newText.length - 1)];
+        self.textView.text = newText;
+}
+
+-(NSString *)reverseWord:(NSString *)oldWord{
+    NSMutableString *newWord = [NSMutableString new];
+    
+    for (int i = 0; i < oldWord.length; i++) {
+        char c = [oldWord characterAtIndex:oldWord.length-1-i];
+        [newWord appendFormat:@"%c", c];
+    }
+    return newWord;
+}
+
+- (NSString *)addHastagToWord: (NSString *)word{
+    if([word containsString:@"#"]){
+        return word;
+    } else {
+    return [NSString stringWithFormat:@"#%@", word];
+    }
+}
+
+- (IBAction)onReverseButtonPressed:(UIButton *)sender {
+    NSString *oldText = self.textView.text;
+    NSMutableString *newText = [NSMutableString new];
+    
+    NSArray *wordsArray = [oldText componentsSeparatedByString: @" "];
+    for (int i = 0; i < wordsArray.count; i++) {
+        NSString *word = wordsArray[i];
+        // if contains hashtag, do not change
+        // if no hashtag, reverse letters
+        if (![word containsString:@"#"]) {
+            word = [self reverseWord:word];
+        }
+        [newText appendString:word];
+        [newText appendString:@" "];
+    }
+    // strip out trailing space
+    newText = [newText substringToIndex:(newText.length - 1)];
+    self.textView.text = newText;
+}
+
+
+//    [wordsArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        NSString *word = (NSString *) obj;
+//        if (index % 2 == 0) {
+//            NSLog(@"")
+//        }
+//    }]
 
 
 @end
